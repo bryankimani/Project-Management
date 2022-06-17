bfd960e5bcd6b6588eb1ed7026c5232b10284e54
Bryan Kimani <brnkimani@gmail.com>
2022-06-17 11:14:26 +0300

    Add Project and Client GraphQL queries
    MongoDB used as the database

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const colors = require("colors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 9000;
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+  console.log(process.env.NODE_ENV === "development");
+});
